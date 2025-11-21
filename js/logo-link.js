8995fd1a6ef2981a634f4295835140d968f0afd6
NehaV20 <n.verma@quantxpress.com>
2025-11-21 14:18:57 +0530

    Deployed 63981d1 with MkDocs version: 1.6.1

--- js/logo-link.js
+++ js/logo-link.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const logoLink = document.querySelector("a.md-logo, a.md-header-nav__title");
     
     if (logoLink) {
-        logoLink.href = "/BlitzTrader-API-Documentation/0.Welcome.html"; 
+        logoLink.href = "/BlitzTrader-API-Documentation/index.html"; 
     }
 });
