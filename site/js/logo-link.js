63981d16ff2282080770f908d98538ae54d1b14b
NehaV20 <n.verma@quantxpress.com>
2025-11-21 13:55:21 +0530

    Fixed

--- site/js/logo-link.js
+++ site/js/logo-link.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the logo link in Material theme
+    
     const logoLink = document.querySelector("a.md-logo, a.md-header-nav__title");
     
     if (logoLink) {
-        // Use MkDocs-style URL (folder-style)
-        logoLink.href = "/0.Welcome/";  // Change this if you rename the home page to index.md, then use "/"
+        logoLink.href = "/BlitzTrader-API-Documentation/0.Welcome.html"; 
     }
 });
